2cd3717f8ae71ea74464a2772e85093af315d379
Ever Cabarcas <evercm95@hotmail.com>
2017-11-30 13:49:08 -0500

    cuarto commit

--- models/estudiante.js
+++ models/estudiante.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 var schema = new Schema({
     nombre: {type: String, required: true},
     codigo: {type: String, required: true},
-    programa: {type: Schema.types.ObjectId, required: true, ref: 'Programa'}
+    programa: {type: Schema.Types.ObjectId, required: true, ref: 'Programa'}
 });
 
 module.export = mongoose.model('Estudiante', schema);
